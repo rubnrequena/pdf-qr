f81241a5fd1753e9b711b47de460d537fd845585
Ruben Requena <rubnrequena@gmail.com>
2021-03-17 00:17:10 +0100

    puerto y host

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(4000, "149.28.253.254");
 }
 bootstrap();
