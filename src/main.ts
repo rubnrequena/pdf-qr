34a227962d15ec8a0990ad17e1c24c4367c89be7
Ruben Requena <rubnrequena@gmail.com>
2021-03-17 11:26:20 +0100

    entorno de host

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+const { HOST = "127.0.0.1" } = process.env;
+
+  await app.listen(3050, HOST);
   const app = await NestFactory.create(AppModule);
   await app.listen(3050, "149.28.253.254");
 }
